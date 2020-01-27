41047ff95071e75c334ecef691387489c27886d1
Alexander Bonawitz <Alexander.L.Bonawitz@gmail.com>
2020-01-27 16:58:17 -0600

    finished second problem for second exericse

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args){
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %3.4s", pi);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %3.4s", pi);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.next();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
     }
 }
